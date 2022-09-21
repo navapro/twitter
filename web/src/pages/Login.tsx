d48fd7f71590cafcd8e391258d39c5f41c21d44b
Naveen <navaneethk002@gmail.com>
2022-09-21 15:38:35 -0500

    added basic styling for login and signup pages

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -3,7 +3,8 @@ import { gql, useMutation } from "@apollo/client";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
-
+import TwitterLogo from "../styles/assets/twitter-logo.png";
+import { Link } from "react-router-dom";
 const LOGIN_MUTATION = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -37,7 +38,13 @@ const Login = () => {
 
   return (
     <div>
-      <h1>login</h1>
+      <img
+        src={TwitterLogo}
+        alt="logo"
+        style={{ width: "50px" }}
+        className="logo"
+      ></img>
+      <h3>Login to Fake Twitter</h3>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
@@ -58,9 +65,15 @@ const Login = () => {
           <Field name="password" type="password" placeholder="Password" />
           <ErrorMessage name="password" component={"div"} />
 
-          <button type="submit">Login</button>
+          <button type="submit" className="login-button">
+            <span>Login</span>
+          </button>
         </Form>
       </Formik>
+      <div className="register">
+        <h4>Don't have an account ?</h4>
+        <Link to="/signup">Sign up</Link>
+      </div>
     </div>
   );
 };

c0fbd6f217b949dd883dcf54b67bcabdab294c78
Naveen <navaneethk002@gmail.com>
2022-09-20 21:05:27 -0500

    starting authentication

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,31 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
 import React from "react";
 import "./App.css";
 import Users from "./components/Users";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
+import { setContext } from "apollo-link-context";
 
+const httpLink = new HttpLink({ uri: "http://localhost:4000" });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: link as any,
   cache: new InMemoryCache(),
 });
 
